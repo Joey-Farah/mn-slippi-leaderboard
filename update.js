0c526d7f4006811ca88c069dfbf893b54c92fd51
bschoenfeld16 <b.schoenfeld16@gmail.com>
2025-06-01 14:48:50 +0000

    syncing with parent fork

--- update.js
+++ update.js
@@ -2,38 +2,54 @@ const { request, gql } = require('graphql-request');
 const fs = require('fs/promises');
 const codes = require('./codes.json');
 const query = gql`
-query AccountManagementPageQuery($cc: String!) {
-  getConnectCode(code: $cc) {
-    user {
-      displayName
-      connectCode {
-        code
-      }
-      rankedNetplayProfile {
-        id
-        ratingOrdinal
-        ratingUpdateCount
-        wins
-        losses
-        dailyGlobalPlacement
-        dailyRegionalPlacement
-        continent
-        characters {
-          character
-          gameCount
-        }
+query UserProfilePageQuery($cc: String, $uid: String) {
+  getUser(fbUid: $uid, connectCode: $cc) {
+    displayName
+    connectCode {
+      code
+    }
+    rankedNetplayProfile {
+      id
+      ratingOrdinal
+      ratingUpdateCount
+      wins
+      losses
+      dailyGlobalPlacement
+      dailyRegionalPlacement
+      continent
+      characters {
+        character
+        gameCount
       }
     }
   }
 }
 `;
-const endpoint = 'https://gql-gateway-2-dot-slippi.uc.r.appspot.com/graphql';
-const getPlayerData = async (cc) => {
-  const variables = { cc: cc.toUpperCase() };
-  const data = await request(endpoint, query, variables);
-  return data;
-}
+
 const timeout = async ms => new Promise((resolve) => setTimeout(resolve, ms));
+const endpoint = 'https://internal.slippi.gg/graphql';
+const getPlayerData = async (rawCC) => {
+  if (!rawCC.includes("#")) {
+    return { getConnectCode: null }
+  }
+  const cc = rawCC.toUpperCase();
+  const variables = { cc };
+  let rtCount = 0;
+  while (true) {
+    try {
+      const data = await request(endpoint, query, variables);
+      const user = data.getUser;
+      const getConnectCode = user && { user };
+      return { getConnectCode };
+    }
+    catch (e) {
+      console.log(e);
+      rtCount += 1;
+      console.log("Retrying", { cc, rtCount });
+      await timeout(5000);
+    }
+  }
+}
 const getUnixTimestamp = () => Math.floor(Date.now() / 1000);
 const main = async () => {
   const data = {};
